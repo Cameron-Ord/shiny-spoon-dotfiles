ba6c8fff8e8193ca9e3ee2a3fc2d3a1be2f8d264
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-12-07 05:07:25 -0700

    fix script

--- install.sh
+++ install.sh
@@ -3,26 +3,31 @@ sudo cp contents/dnf.conf /etc/dnf/dnf.conf
 sudo dnf install picom clang lldb llvm gdb gcc python python-pip nodejs npm git i3 sddm make cmake unzip ninja-build gettext curl glibc-gconv-extra dmenu tmux alacritty nano lua lua-devel
 
 dir=$(pwd)
-git clone git clone https://github.com/neovim/neovim ~/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git clone https://github.com/neovim/neovim ~/neovim
+cd ~/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install 
 cd $dir
 
-cp -r contents/i3 ~/.config/
-cp -r contents/alacritty ~/.config/
-cp -r contents/picom ~/.config/
-cp contents/.tmux.conf ~/
+mkdir -p ~/.config/
+config_dir=$HOME/.config/
 
-cp -r contents/nvim ~/.config/
-cd ~/.config/nvim && sudo unzip pack.zip
+sudo cp -r contents/i3 $config_dir
+sudo cp -r contents/alacritty $config_dir
+sudo cp -r contents/picom $config_dir
+sudo cp contents/.tmux.conf $HOME
+
+sudo cp -r contents/nvim $config_dir
+cd ~/.config/nvim
+pack_dir=$(pwd)/pack
+if [ -d $pack_dir ]; then
+ sudo rm -r $pack_dir
+fi
+sudo unzip pack.zip
 cd $dir
 
-mkdir -p ~/wallpapers
-cd ~/wallpapers
+mkdir -p ~/wallpapers/
+cd ~/wallpapers/
 
 curl -O https://w.wallhaven.cc/full/yx/wallhaven-yxdrex.png
 cp *.png ~/wallpapers/
-
-sudo systemctl enable sddm
-sudo systemctl start sddm
 
